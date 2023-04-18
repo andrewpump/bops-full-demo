f32db41edd5cc6e9d48ac392076150f2f2b49c6e
kapil <duckatgit@gmail.com>
2023-04-18 19:25:28 +0530

    Created new demo app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {AiAssistant} from 'duckdevatgit-layer';
+import './App.css';
+import DemoImage from "./assets/images/demoImage.png"
+
+function App() {
+  const getListItem  = [
+    {
+      "title": "Purchase more product",
+      "subtitle": "PRINGLES SOUR CREAM & ONION PO"
+    },
+    {
+      "title": "Purchase more product",
+      "subtitle": "PRINGLES  SNK STK 3 FLV CRISP"
+    },
+    {
+      "title": "Purchase more product",
+      "subtitle": "PRINGLES  SNK STK 3 FLV CRISP"
+    },
+    {
+      "title": "Purchase more product",
+      "subtitle": "PRINGLES  SNK STK 3 FLV CRISP"
+    },
+  ];
+  return (
+    <div className="main-background">
+    <AiAssistant itemList={getListItem} color="#7b6cf3" image={DemoImage} />
+    <h1>BOPS React web app in this background area!</h1>
+  </div>
+  );
+}
+
+export default App;
